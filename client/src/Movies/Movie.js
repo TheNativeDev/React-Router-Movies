7be9d92ff4d771801386376129b7c47e57c580aa
LezaJackson <53628558+LezaJackson@users.noreply.github.com>
2019-11-12 03:01:37 -0800

    mvp and stretch completed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,57 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
 
-const Movie = (props) => {
-  const [movie, setMovie] = useState();
- 
+const Movie = props => {
+  const [movieInfo, setMovieInfo] = useState(null);
+
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
+
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        setMovieInfo(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [props.match.params]);
 
-  },[]);
-  
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movieInfo);
+  };
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
-
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+  return movieInfo ? (
+    <MovieCard movieInfo={movieInfo} saveMovie={saveMovie} />
+  ) : (
+    <div>Loading movie information...</div>
   );
-}
+};
 
 export default Movie;
